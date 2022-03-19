8492c75f2fe794b8cb887b897b43edc79d06c346
PrakharYadav7777 <101917060+PrakharYadav7777@users.noreply.github.com>
2022-03-19 20:50:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const mineflayer = require('mineflayer')
-const fs = require('fs');
-let rawdata = fs.readFileSync('config.json');
-let data = JSON.parse(rawdata);
+var mineflayer = require('mineflayer');
+
+var host = "OneBlockWM2R.aternos.me";
+var port = 21728;
+var username = "Bot"
+var moveinterval = 2; // 2 second movement interval
+var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+
+// code start
+var bot = mineflayer.createBot({
+  host: host,
+  port: port,       // optional
+  username: username,
+  password: password
+});
+
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
 var actions = [ 'forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
-var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
-var host = data["ip"];
-var username = data["name"]
-var bot = mineflayer.createBot({
-  host: host,
-  username: username
-});
+
 function getRandomArbitrary(min, max) {
        return Math.random() * (max - min) + min;
 
 }
-bot.on('login',function(){
-	console.log("Logged In")
+
+var sleep = require('sleep');
+
+bot.on('chat', function(username, message) {
+  if (username === bot.username) return;
+  console.log(message);
 });
+
+bot.on('health',function() {
+    if(bot.food < 15) {
+        bot.activateItem();
+        console.log("Ate something");
+    }
+});
+
 bot.on('time', function() {
     if (connected <1) {
         return;
     }
     if (lasttime<0) {
         lasttime = bot.time.age;
+        console.log("Age set to " + lasttime)
     } else {
         var randomadd = Math.random() * maxrandom * 20;
         var interval = moveinterval*20 + randomadd;
@@ -36,14 +54,18 @@ bot.on('time', function() {
             if (moving == 1) {
                 bot.setControlState(lastaction,false);
                 moving = 0;
+                console.log("Stopped moving after " + (interval/20) + " seconds");
                 lasttime = bot.time.age;
             } else {
                 var yaw = Math.random()*pi - (0.5*pi);
                 var pitch = Math.random()*pi - (0.5*pi);
                 bot.look(yaw,pitch,false);
+                console.log("Changed looking direction to yaw " + yaw + " and pitch " + pitch);
+
                 lastaction = actions[Math.floor(Math.random() * actions.length)];
                 bot.setControlState(lastaction,true);
                 moving = 1;
+                console.log("Started moving " + lastaction +" after " + (interval/20) + "seconds");
                 lasttime = bot.time.age;
                 bot.activateItem();
             }
@@ -55,3 +77,18 @@ bot.on('spawn',function() {
     connected=1;
 });
 
+bot.on('end', function () {
+    console.log("Disconnected. Waiting 10 seconds")
+    bot.quit();
+    sleep.sleep(10);
+    lasttime = -1;
+    moving = 0;
+    connected=0;
+    bot = mineflayer.createBot({
+        host: host,
+        port: port,       // optional
+        username: username,
+        password: password
+    });
+    console.log("reconnected.")
+});
